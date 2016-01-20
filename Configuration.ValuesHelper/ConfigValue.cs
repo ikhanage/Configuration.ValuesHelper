701a5f4ab71bd90d51050fb3111b7ade82855efc
ikhanage <quiteimran@yahoo.co.uk>
2016-01-20 22:56:31 +0000

    Commiting code.

---
+++ Configuration.ValuesHelper/ConfigValue.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Configuration;
+
+namespace Configuration.ValuesHelper
+{
+    public static class ConfigValue
+    {
+        public static AppSettingsReader reader;
+        static ConfigValue()
+        {
+            reader = new AppSettingsReader();            
+        }
+        public static string GetAsString(string key)
+        {
+            return reader.GetValue(key, typeof(string)) as string;
+        }
+
+        public static bool GetAsBool(string key)
+        {
+            var nullBool = reader.GetValue(key, typeof(bool?)) as bool?;
+
+            if (!nullBool.HasValue)
+                throw new ConfigurationException(string.Format("The value of {0} was not a bool.", key));
+
+            return nullBool.Value;
+        }
+
+        public static bool? GetAsNullableBool(string key)
+        {
+            return reader.GetValue(key, typeof(bool?)) as bool?;
+        }
+
+        public static int GetAsInt(string key)
+        {
+            var nullString = reader.GetValue(key, typeof(string)) as string;
+
+            int @int;
+            var isBool = Int32.TryParse(nullString, out @int);
+
+            if (!isBool)
+                throw new ConfigurationException(string.Format("The value of {0} was not an int.", key));
+
+            return @int;
+        }
+
+        public static int? GetAsNullableInt(string key)
+        {
+            var nullString = reader.GetValue(key, typeof(string)) as string;
+
+            int @int;
+            if (!Int32.TryParse(nullString, out @int)) return null;
+
+            return @int;
+        }
+
+        public static double GetAsDouble(string key)
+        {
+            var nullString = reader.GetValue(key, typeof(string)) as string;
+
+            double @double;
+            if (!double.TryParse(nullString, out @double))
+                throw new ConfigurationException(string.Format("The value of {0} was not a double.", key));
+
+            return @double;
+        }
+
+        public static double? GetAsNullableDouble(string key)
+        {
+            var nullString = reader.GetValue(key, typeof(string)) as string;
+
+            double @double;
+            if (!double.TryParse(nullString, out @double)) return null;
+
+            return @double;
+        }
+
+        public static DateTime GetAsDateTime(string key)
+        {
+            var dateTimeString = reader.GetValue(key, typeof(string)) as string;
+
+            DateTime dateTime;
+
+            if(!DateTime.TryParse(dateTimeString, out dateTime))
+                throw new ConfigurationException(string.Format("The value of {0} was not a datetime.", key));
+
+            return dateTime;
+        }
+
+        public static DateTime? GetAsNullableDateTime(string key)
+        {
+            var dateTimeString = reader.GetValue(key, typeof(string)) as string;
+
+            DateTime dateTime;
+
+            if (!DateTime.TryParse(dateTimeString, out dateTime))
+                return null;
+
+            return dateTime;
+        }
+    }
+}
---
+++ Configuration.ValuesHelperTestsProject/UnitTest1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Configuration.ValuesHelper;
+using System.Configuration;
+using System;
+
+namespace Configuration.ValuesHelperTestsProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        const string intKey = "TestInt";
+        const string doubleKey = "TestDouble";
+        const string stringKey = "TestString";
+        const string dmykey = "TestDateStringddmmyyyy";
+        const string dmyhmkey = "TestDateStringddmmyyyyhhmm";
+
+        [TestMethod]
+        public void CorrectInt()
+        {
+            var expectedResult = 1;
+
+            var actualResult = ConfigValue.GetAsInt(intKey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigurationException), "The value of TestString was not an int.")]
+        public void NotInt()
+        {
+            var actualResult = ConfigValue.GetAsInt(stringKey);           
+        }
+
+        [TestMethod]
+        public void CorrectIntNull()
+        {
+            var expectedResult = 1;
+
+            var actualResult = ConfigValue.GetAsNullableInt(intKey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void NotIntNull()
+        {
+            var actualResult = ConfigValue.GetAsNullableInt(stringKey);
+
+            Assert.AreEqual(null, actualResult);
+        }
+
+        [TestMethod]
+        public void CorrectDouble()
+        {
+            var expectedResult = 5.0;
+
+            var actualResult = ConfigValue.GetAsDouble(doubleKey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigurationException), "The value of TestString was not a double.")]
+        public void IncorrectDouble()
+        {
+            var actualResult = ConfigValue.GetAsDouble(stringKey);
+        }
+
+        [TestMethod]
+        public void CorrectDoubleNull()
+        {
+            var expectedResult = 5.0;
+
+            var actualResult = ConfigValue.GetAsNullableDouble(doubleKey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void IncorrectDoubleNull()
+        {
+            var actualResult = ConfigValue.GetAsNullableDouble(stringKey);
+
+            Assert.AreEqual(null, actualResult);
+        }
+
+        [TestMethod]
+        public void CorrectDateTime()
+        {
+            var expectedResult = DateTime.Parse("15/12/1990 00:00:00");
+
+            var actualResult = ConfigValue.GetAsDateTime(dmykey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigurationException), "The value of TestString was not a double.")]
+        public void IncorrectDateTime()
+        {
+            var actualResult = ConfigValue.GetAsDateTime(stringKey);
+        }
+
+        [TestMethod]
+        public void CorrectDateTimeNull()
+        {
+            var expectedResult = DateTime.Parse("15/12/1990 00:00:00");
+
+            var actualResult = ConfigValue.GetAsNullableDateTime(dmykey);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void IncorrectDateTimeNull()
+        {
+            var actualResult = ConfigValue.GetAsNullableDateTime(stringKey);
+
+            Assert.AreEqual(null, actualResult);
+        }
+    }
+}
